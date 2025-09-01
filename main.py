a1ba4e5ec79b2ce9a0781ca99386ebdf451522e8
SamuelSalcedo <salcedolucerosmauel2@gmail.com>
2025-08-31 20:28:22 -0600

    tabla funcionando

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #Creditos: CodeWithCurious by Shivakshi Chouhan 
 #tkinter se usa para crear GUI botones, labels, input etc
 import tkinter as tk
+import json
+
 #ttk  = treeview widget para ver la informacion en una tabla
 #messagebox es apra mostrar mensajes pop-up
 from tkinter import ttk, messagebox
@@ -49,74 +51,38 @@ root.geometry("850x500")
 #los siguientes elementos usan el 1 como padre
 
 gastos_tree = [
-    ("", "end", 1, "Comida",("","Descripcion", "Cantidad")),
-    ("", "end", 2, "Gastos",("","Descripcion", "Cantidad")),
-    ("", "end", 3, "Transporte",("","Descripcion", "Cantidad")),
-    ("", "end", 4, "Ocio",("","Descripcion", "Cantidad")),
-    ("", "end", 5, "Gasto Hormiga",("","Descripcion", "Cantidad")), 
-          ]
+    ("", "end", 1, "Comida"),
+    ("", "end", 2, "Gastos"),
+    ("", "end", 3, "Transporte"),
+    ("", "end", 4, "Ocio"),
+    ("", "end", 5, "Gasto Hormiga"), 
+]
+
 #cada gasto esta formado por una descripcion, categoria y cantidad 
-
-
-
-def agregar_gastos():
-    
-    desc = descripcion.get()
-    cat = categoria.get()
-    cant = cantidad.get()
-    
-    print(desc)
-    print(cat)
-    print(cant)
-    #Valida que todos los campos esten
-    if not desc or not cat or not cant:
-        messagebox.showerror("Error de campos","Debe de completar todos los campos")
-        return
-    
-    #valida que se ingrese un numero
-    try:
-        cant = float(cant)
-    except ValueError:
-        messagebox.showerror("Error de campos", "Debe de ingresar un numero")
-        return
-    
-    #Agrega la lista de los gastos como una tupla
-    #gastos_tree.append((desc,cat,cant))
-    
-    descripcion.delete(0, tk.END)
-    categoria.delete(0, tk.END)
-    cantidad.delete(0, tk.END)
-
+total =0
 
 input_frame = ttk.LabelFrame(root, text="Rellena los campos", padding=(20, 10))
 input_frame.grid(row=0, column=0, padx=(20, 10), pady=(20, 10), sticky="nsew")
 
 descripcion = ttk.Entry(input_frame)
 descripcion.insert(0, " ")
-descripcion.grid(row=0, column=0, padx=3, pady=(0, 15), sticky="ew")
+descripcion.grid(row=0, column=0, padx=3, pady=(0, 15), sticky="w")
 
 categoria = ttk.Combobox(input_frame, values=combo_list)
 categoria.current(0)
-categoria.grid(row=1, column=0, padx=5, pady=10,  sticky="ew")
+categoria.grid(row=1, column=0, padx=5, pady=10,  sticky="w")
+categoria.set("comida")
 
 cantidad = ttk.Entry(input_frame)
 cantidad.insert(0, "0.0")
-cantidad.grid(row=2, column=0, padx=5, pady=(0, 15), sticky="ew")
+cantidad.grid(row=2, column=0, padx=5, pady=(0, 15), sticky="w")
 
 # Separator
 separator = ttk.Separator(root)
-separator.grid(row=2, column=0, padx=(20, 10), pady=10, sticky="ew")
-
-agregar_btn = ttk.Button(input_frame, text="Agregar", command=agregar_gastos)
-agregar_btn.grid(row=4, column=0, padx=5, pady=10, sticky="nsew")
+separator.grid(row=2, column=0, padx=(20, 10), pady=10, sticky="w")
 
 
-#agregar_btn = tk.Button(root, text="Agregar Gastos", command=agregar_gastos, bg="#4CAF50", fg="white")
-#agregar_btn.pack(pady=10)
-        
-#crear la tabla de como se ven los gastos
-#Crea una tabla con 4 columas con: numero, descripcion, categoria, cantidad 
-    
+
 # Panedwindow
 paned = ttk.PanedWindow(root)
 paned.grid(row=0, column=2, pady=(35, 5), sticky="nsew", rowspan=2)
@@ -150,23 +116,55 @@ treeview.heading("#0", text="Categoria", anchor="center")
 treeview.heading(1, text="#", anchor="center")
 treeview.heading(2, text="Descripcion", anchor="center")
 treeview.heading(3, text="Cantidad", anchor="center")
-treeview.heading(4, text="Total", anchor="center")
+treeview.heading(4, text= "Total", anchor="center")
 
-#mostrar de forma predeterminada el contenido del tree vacio
-for item in gastos_tree:
-    treeview.insert(parent=item[0], index=item[1], iid=item[2], text=item[3], values=item[4])
-    if item[0] == "" or item[2] in (8, 12):
-       treeview.item(item[2], open=True) # Open parents
-
-
-
-    #muestra la cantidad total de los gastos
-#total_resultado = tk.Label(root, text="Total: $0.00", font=("Arial", 12, "bold"), bg="white", fg="green")
-#total_resultado.pack(pady=10)
-    
 #exportar a excel la lista con el total 
+for item in gastos_tree:
+    treeview.insert(parent=item[0], index=item[1], iid=item[2], text=item[3])
+    if item[0] == "" or item[2] in (8, 12):
+        treeview.item(item[2], open=True) # Open parents
 
+treeview.insert("", "end", values=("", "", "", total))
+        
+contador = 0
+def agregar_gastos():
+    #index para cada uno de los gastos
+    gastos ={
+        "comida" : 1,
+        "Gastos" : 2,
+        "Transporte":3,
+        "Ocio":4,
+        "Gasto Hormiga":5
+    }
+    global contador
+    global total
+    desc = descripcion.get()
+    cat = categoria.get()
+    cant = cantidad.get()
+    id_padre = gastos.get(cat)
+    #Valida que todos los campos esten
 
+    if not desc or not cat or not cant:
+        messagebox.showerror("Error de campos","Debe de completar todos los campos")
+        return
+    else:
+            #valida que se ingrese un numero
+        try:
+            cant = float(cant)
+            
+        except ValueError:
+            messagebox.showerror("Error de campos", "Debe de ingresar un numero")
+            return
+        contador += 1
+        
+        if id_padre:
+            total += cant
+            treeview.insert(id_padre, "end", values=(contador,desc, cant, total))
     
+    descripcion.delete(0, tk.END)
+    cantidad.delete(0, tk.END)
+    
+agregar_btn = ttk.Button(input_frame, text="Agregar", command=agregar_gastos)
+agregar_btn.grid(row=4, column=0, padx=5, pady=10, sticky="nsew")
 #Este en loop la ventana
 root.mainloop()
